ed2f83264e98c64e6cadcaed9e9b05ae3b9a4d84
Oliver Davies <oliver@oliverdavies.uk>
2022-01-26 13:33:39 +0000

    Force a failing test

--- hello_test.go
+++ hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	got := Hello("Oliver")
-	want := "Hello, Oliver"
+	want := "Hello, failure"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
