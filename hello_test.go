d4bc9ae15fead5634ea9401b2e1e2a31b0ecca38
Oliver Davies <oliver@oliverdavies.uk>
2022-01-27 18:00:00 +0000

    Extract a custom assertion

--- hello_test.go
+++ hello_test.go
@@ -3,21 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("saying hello to people", func (t *testing.T) {
-		got := Hello("Oliver")
-		want := "Hello, Oliver"
+	assertCorrectMessage := func(t testing.TB, got string, want string) {
+		t.Helper()
 
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
+	}
+
+	t.Run("saying hello to people", func (t *testing.T) {
+		got := Hello("Oliver")
+		want := "Hello, Oliver"
+
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func (t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
