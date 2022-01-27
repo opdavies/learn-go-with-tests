a92fffd81e67366fa728cb58142c7302345bdccc
Oliver Davies <oliver@oliverdavies.uk>
2022-01-27 00:11:38 +0000

    Add Welsh

--- hello_test.go
+++ hello_test.go
@@ -12,15 +12,22 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func (t *testing.T) {
-		got := Hello("Oliver")
+		got := Hello("Oliver", "")
 		want := "Hello, Oliver"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func (t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Welsh", func (t *testing.T) {
+		got := Hello("Oliver", "Welsh")
+		want := "Helo, Oliver"
 
 		assertCorrectMessage(t, got, want)
 	})
